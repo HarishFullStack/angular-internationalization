cc8e45bd0c6e5f5bb8e3a53cb74c764d7338229f
HarishFullStack <harishgupta.dev@gmail.com>
2024-09-17 21:28:20 +0530

    added back button

--- src/app/financial-details/financial-details.component.ts
+++ src/app/financial-details/financial-details.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-financial-details',
   templateUrl: './financial-details.component.html',
   styleUrl: './financial-details.component.scss',
 })
-export class FinancialDetailsComponent {}
+export class FinancialDetailsComponent {
+  constructor(private router: Router) {}
+
+  back() {
+    this.router.navigate(['/']);
+  }
+}
